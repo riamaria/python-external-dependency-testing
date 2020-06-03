f6ac427f80ad79e74689f9e5540e6bf4eae87f80
Maria Mora <mmora@crunchyroll.com>
2020-06-03 09:53:41 +0100

    Add setup documentation and initial program

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # python-external-dependency-testing
 Course material for WWCode Connect Digital 2020
+
+## Requirements
+
+- python 3.8+
+- working internet connection for `pip` operations and calling the Advice Slip API
+
+## Setting up
+
+0. (Optional) If you are familiar with `virtualenv`, you may use `venv` as the virtual environment directory (added to `.gitignore`).
+
+    ```
+      python -m venv venv
+    ```
+
+    Make sure to activate your virtual environment afterwards. Command for activating the virtual environment will depend on your system and terminal.
+
+    ```
+      source venv/Scripts/activate
+    ```
+
+1. Download dependencies per `requirements.txt`
+
+    ```
+      python -m pip install -r requirements.txt
+    ```
+
+2. Run `main.py` to make sure that things are running on your end
+
+
+    ```
+      python main.py
+    ```
+
+## Credits
+
+[Advice Slip API](https://api.adviceslip.com/) ([@adviceslip](https://twitter.com/adviceslip)) provided by [@tomkiss](https://twitter.com/tomkiss)
---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import jsonschema
+
+import requests
+
+# This is used to validate the API response
+get_advice_schema = {
+    "type": "object",
+    "additionalProperties": False,
+    "required": ["slip"],
+    "properties": {
+        "slip": {
+            "type": "object",
+            "additionalProperties": False,
+            "required": ["id", "advice"],
+            "properties": {
+                "id": {"type": "integer"},
+                "advice": {"type": "string"}
+            }
+        }
+    }
+}
+
+
+def get_advice():
+    # Make API request
+    response = requests.get("https://api.adviceslip.com/advice")
+    if response.status_code != 200:
+        raise Exception("Error trying to load API")
+
+    # Make sure that the advice can be retrieved from the response
+    response_body = response.json()
+    try:
+        jsonschema.validate(instance=response_body, schema=get_advice_schema)
+    except jsonschema.exceptions.ValidationError:
+        raise Exception(
+            f"Unexpected JSON response format. Response: {response.text}")
+
+    # Print out advice
+    advice = response_body["slip"]["advice"]
+
+    return f"Your advice: {advice}"
+
+
+if __name__ == "__main__":
+    print(get_advice())
